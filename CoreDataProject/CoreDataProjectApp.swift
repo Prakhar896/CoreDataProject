c77189c2f7573e9297b6f5b3f2c69086a2eb1f27
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-08-23 23:36:48 +0800

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Prakhar Trivedi on 23/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
